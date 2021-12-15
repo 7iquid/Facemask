81aa114818a0929acbad3561f111d5e2b2214e32
tavs <82376053+7iquid@users.noreply.github.com>
2021-12-16 00:42:27 +0800

    Update models.py

--- api/models.py
+++ api/models.py
@@ -6,6 +6,7 @@ class Machine(models.Model):
 	date = models.DateTimeField(auto_now_add=True)
 	machine_no = models.IntegerField(null=False, default=1)
 	machine_status = models.BooleanField(null=False, default=False)
+	status_update = models.DateTimeField(default=now, blank=True)
 
 	def __str__(self):
 		return self.machine_no
