fc51105f7deff3d6045659a6f5e5215eccf9b936
tavs <82376053+7iquid@users.noreply.github.com>
2022-01-13 15:48:41 +0800

    Update facemaskAjax.js

--- MainPage/static/js/facemaskAjax.js
+++ MainPage/static/js/facemaskAjax.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  let url2 = 'http://192.168.1.150:8000/api';
-  // let url2 = 'http://127.0.0.1:8000/api/'; 
+  // let url2 = 'http://192.168.1.150:8000/api';
+  let url2 = 'http://127.0.0.1:8000/api/'; 
     $('#mc1form').hide();
     $('#mc2form').hide();
     $('#mc3form').hide();
@@ -77,7 +77,7 @@ $(document).ready(function(){
 
       },
       error: function(XMLHttpRequest, textStatus, errorThrown) { 
-      
+      url2 = 'http://192.168.1.150:8000/api';
       }       
 
       })
